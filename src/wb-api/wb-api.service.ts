258eeb56fda5c5759c4acf4100a4c9b36f632089
Vadim Osyukov <osyukov.v@n-t.io>
2021-11-29 00:33:11 +0300

    init

--- src/wb-api/wb-api.service.ts
+++ src/wb-api/wb-api.service.ts
@@ -86,7 +86,7 @@ export class WbApiService {
           dateFrom: moment().subtract(4, 'M').format('YYYY-MM-DD'),
           dateTo: moment().format('YYYY-MM-DD'),
           rrdid: rrdId,
-          limit: 10000,
+          limit: 1000,
         },
         responseType: 'json',
       })
