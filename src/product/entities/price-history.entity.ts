5034971d24d64b59ac6f5b774d81f61f518c1127
Vadim Osyukov <osyukov.v@n-t.io>
2021-12-07 16:26:53 +0300

    fix

--- src/product/entities/price-history.entity.ts
+++ src/product/entities/price-history.entity.ts
@@ -4,12 +4,16 @@ import { ShopEntity } from '../../shop/entities/shop.entity';
 export const TABLE_NAME = 'cost_price_history';
 
 @Entity(TABLE_NAME)
-@Index(['barcode', 'createdAt', 'updatedAt'])
+@Index(['barcode', 'shopId', 'createdAt', 'updatedAt'])
+@Index(['shopId'])
 export class PriceHistoryEntity {
   public static tableName: string = TABLE_NAME;
 
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
+  shopId: string;
 
   @Column({ nullable: false })
   barcode: string;
