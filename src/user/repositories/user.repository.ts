bc57acb53655acc23654f60f95309456c6714d59
Vadim Osyukov <osyukov.v@n-t.io>
2021-12-12 22:47:59 +0300

    fix

--- src/user/repositories/user.repository.ts
+++ src/user/repositories/user.repository.ts
@@ -7,4 +7,9 @@ export class UserRepository extends Repository<UserEntity> {
     const result = await this.findOne({ where: { tgId: id }, relations: ['shop'] });
     return result ?? null;
   }
+
+  public async updateSubscriptionExpirationDate(userId: string, date: Date): Promise<void> {
+    await this.update({ id: userId }, { subscriptionExpirationDate: date });
+    return;
+  }
 }
