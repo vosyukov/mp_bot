be952ee1a7cc9f4d5f4e8f4d32ca22b5cf46ce1c
Vadim Osyukov <osyukov.v@n-t.io>
2021-12-21 00:35:36 +0300

    fix

--- src/wb_stats/entities/sales-report.entity.ts
+++ src/wb_stats/entities/sales-report.entity.ts
@@ -1,10 +1,10 @@
-import { Column, Entity, Index, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, Unique } from 'typeorm';
 import { ShopEntity } from '../../shop/entities/shop.entity';
 
 export const TABLE_NAME = 'sales_reports';
 
 @Entity(TABLE_NAME)
-@Index(['barcode', 'shopId', 'rrDt'])
+@Unique(['barcode', 'shopId', 'rrDt'])
 export class SalesReportEntity {
   static tableName: string = TABLE_NAME;
 
