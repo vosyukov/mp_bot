0bf95a07f6ea3b30f8339f05f68828322ca71081
Vadim Osyukov <osyukov.v@n-t.io>
2022-01-15 18:13:10 +0300

    fix

--- api/ormconfig.js
+++ api/ormconfig.js
@@ -10,7 +10,7 @@ const options = {
   entities: ['./dist/*/entities/*{.ts,.js}', './src/*/entities/*{.ts,.js}'],
   synchronize: true,
   migrations: ['./dist/*/entities/migrations/*{.ts,.js}', './src/*/entities/migrations/*{.ts,.js}'],
-  logging: false,
+  logging: true,
   connectTimeout: 30000,
   cli: { migrationsDir: 'migrations' },
 };
